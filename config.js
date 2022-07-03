14093fa76f1953f1677351545c2747b7d0bcd531
Ayane45121 <108357375+Ayane45121@users.noreply.github.com>
2022-07-04 01:18:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ global.mods = []
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Vinz Cyx'
+global.nameowner = 'Vinz'
 global.numberowner = '6287734910547'
 global.instagram = 'https://instagram.com/_alv.stn'
 global.github = 'https://github.com/Pinzz45'
@@ -15,7 +15,7 @@ global.gopay = '087734910547'
 //=============『 Info Bot 』=================//
 global.namebot = 'Nino Bot'
 global.gc = 'https://chat.whatsapp.com/C0cERkbxNmM2veEQSKPf7D'
-global.web = 'https://github.com/Hyzerr' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.web = 'https://github.com/Pinzz45' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -23,7 +23,7 @@ global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'ꜱʜɪʀᴏ ɪᴋᴀꜱᴀᴋɪ ʙᴏᴛ'
+global.wm = 'ɴɪɴᴏ ʙᴏᴛ'
 global.watermark = wm
 global.wm2 = '                     「 Nino Bot 」'
 global.wm3 = 'Nino Bot'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*Stiker sedang dibuat...*'
 global.packname = '2022'
-global.author = '© Vinz Cyx'
+global.author = '© vyn'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
