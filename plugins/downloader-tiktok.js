30ff235f59fafdb9e8512de2e7d8f84e560e3a8d
MFXZ <109869000+MFXZ@users.noreply.github.com>
2022-08-20 22:01:28 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,12 +1,82 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
-
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih', m)
+/*let axios = require('axios');
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
+handler.command = /^(tik(tok)?(dl)?)$/i
 
+handler.limit = 1
+
+module.exports = handler*/
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=jokowi3periode&url=${args[0]}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { description, author, statistic, link } = json.result
+await conn.sendFile(m.chat, link, 'tt.mp4', `
+▶ ${statistic.playCount} Views
+❤ ${statistic.diggCount} Likes
+🔁 ${statistic.shareCount} Shares
+💬 ${statistic.commentCount} Comments
+- *By:* ${author.nickname} (${author.username})
+- *Desc:*
+${description}
+`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+}
+
+handler.help = ['tiktok <url>']
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(dl)?)$/i
+handler.limit = true
+module.exports = handler/*let axios = require('axios');
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(dl)?)$/i
+
+handler.limit = 1
+
+module.exports = handler*/
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=jokowi3periode&url=${args[0]}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { description, author, statistic, link } = json.result
+await conn.sendFile(m.chat, link, 'tt.mp4', `
+▶ ${statistic.playCount} Views
+❤ ${statistic.diggCount} Likes
+🔁 ${statistic.shareCount} Shares
+💬 ${statistic.commentCount} Comments
+- *By:* ${author.nickname} (${author.username})
+- *Desc:*
+${description}
+`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+}
+
+handler.help = ['tiktok <url>']
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(dl)?)$/i
+handler.limit = true
 module.exports = handler
+
+
+
